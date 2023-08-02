7c9188909f8f5faa3045bea7cd3fb12d73541e00
asifmahmud00 <asif.mahmud00@gmail.com>
2023-08-02 20:29:00 +0600

    My First git commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button class="btn">Bismillahir Rahmanir Rahim</button>
+      <button class="btn btn-primary">Subhanallah</button>
+      <button class="btn btn-secondary">Alhamdulillah</button>
+      <button class="btn btn-accent">La Ilaha Illallah</button>
+      <button class="btn btn-ghost">Allhu Akbar</button>
+      <button class="btn">La hawla Wala Quuata IllaBillah</button>
+      <button class="btn btn-link">Wa lillahil Hamd</button>
     </div>
   );
 }
